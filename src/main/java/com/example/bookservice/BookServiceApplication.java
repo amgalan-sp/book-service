5715c048269d9e7a8cb8099cc2b339663572f4e6
amg <darzhaev@gmail.com>
2022-11-24 02:14:30 +0900

    added

--- src/main/java/com/example/bookservice/BookServiceApplication.java
+++ src/main/java/com/example/bookservice/BookServiceApplication.java
@@ -1,11 +1,13 @@
 package com.example.bookservice;
 
+//import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import service.ApplicationRunner;
+import service.ApplicationRunner;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,9 +17,9 @@ public class BookServiceApplication {
         SpringApplication.run(BookServiceApplication.class, args);
     }
 
-//    @Bean
-//    public ApplicationRunner applicationStartupRunner() {
-//        return new ApplicationRunner();
-//    }
+    @Bean
+    public ApplicationRunner applicationStartupRunner() {
+        return new ApplicationRunner();
+    }
 }
 
